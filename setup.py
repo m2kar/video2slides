e0fe3fcdfcf4b9651992710a9b8b683f6cf54a66
m2kar <zhiqing.rui@gmail.com>
2024-12-01 05:47:14 +0800

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="video2slides",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=[
         # 列出你的包依赖项

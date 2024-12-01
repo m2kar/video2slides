4efde13d462de8e5017bdec88f23e8e57d7152b2
m2kar <zhiqing.rui@gmail.com>
2024-12-01 16:55:48 +0800

    优化场景检测和图片截取功能；更新README，添加字幕生成说明；修改.gitignore以排除临时文件

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 - 从视频中提取场景
 - 生成PPTX幻灯片
+- 语音识别，生成字幕
 - 自定义场景检测参数
 
 ## 安装
@@ -13,17 +14,34 @@
 确保你已经安装了 [Python](https://www.python.org/) 和 [pip](https://pip.pypa.io/):
 
 ```bash
-git clone 
-pip install .
+pip install git+https://github.com/m2kar/video2slides.git
 ```
 
 ## 使用方法
 
 ```bash
-video2slides <输入视频路径> [输出PPTX路径]
+$ video2slides --help
+Usage: video2slides [OPTIONS] VIDEO [OUTPUT]
+
+  Convert video to pptx slides
+
+Arguments:
+  VIDEO     输入视频文件路径  [required]
+  [OUTPUT]  输出pptx文件路径
+
+Options:
+  --srt / --no-srt                是否生成字幕  [default: no-srt]
+  --install-completion [bash|zsh|fish|powershell|pwsh]
+                                  Install completion for the specified shell.
+  --show-completion [bash|zsh|fish|powershell|pwsh]
+                                  Show completion for the specified shell, to
+                                  copy it or customize the installation.
+  --help                          Show this message and exit.
 ```
 
 ### 示例
+
+示例视频文件和PPTX输出可查看`./example/example.mp4`和`./example/example.pptx`
 
 将 `example.mp4` 转换为幻灯片：
 
@@ -31,36 +49,28 @@ video2slides <输入视频路径> [输出PPTX路径]
 video2slides example.mp4
 ```
 
-如果未指定输出路径，默认生成 `example.pptx`。如果文件已存在，将自动添加时间戳：
+如果未指定输出路径，默认生成 `example.pptx`。如果文件已存在，将自动添加时间戳。
 
-```bash
-video2slides example.mp4
-```
-
-指定输出路径：
+也可以指定输出路径：
 
 ```bash
 video2slides example.mp4 output.pptx
 ```
 
-字幕:
+添加字幕:
+
+(需正确安装 openai-whisper库)
 
 ```bash
 # 音轨转换为字幕，放到幻灯片备注:
 
 video2slides example.mp4 --srt
 ```
-## 配置
+
+## 自定义配置
 
 可以通过修改 `video2slides/main.py` 中的 `Config` 类来调整场景检测参数：
 
-```python
-class Config:
-    skip_frames: int = 0
-    num_images: int = 7
-    threshold: float = 0.1
-    min_scene_len: float = 0.5
-```
 
 ## 依赖
 
@@ -69,7 +79,19 @@ class Config:
 - [python-pptx](https://python-pptx.readthedocs.io/)
 - [Pillow](https://python-pillow.org/)
 
-- [openai-whisper](https://github.com/openai/whisper)
+- [可选][openai-whisper](https://github.com/openai/whisper)
+
+## TODO
+- [ ] 优化有幻灯片过渡的分割准确率
+- [ ] 优化包含摄像头的视频的分割准确率
+- [ ] 优化PPT内嵌视频的分割准确率
+
+- [ ] scenedetect切换为api调用，非os.system调用
+- [ ] 提供GUI界面
+- [ ] 提供在线服务
+- [ ] 增加多语言支持
+- [ ] 增加单元测试和集成测试
+- [ ] 开发docker
 
 ## 许可证
 
--- video2slides/main.py
+++ video2slides/main.py
@@ -1,5 +1,6 @@
 # video2slides/main.py
 import os
+import sys
 import shutil
 import typer
 from typing_extensions import Annotated
@@ -10,14 +11,23 @@ import re
 from pptx import Presentation
 from pptx.util import Inches
 from PIL import Image
+import tqdm
+import signal
+import atexit
 
 DEBUG=bool(os.getenv("DEBUG",False))
 
 class Config:
-    skip_frames: int = 5
-    num_images:int = 9
+    skip_interval: int = 0.15 # 跳过的时间间隔 (秒)
+    skip_frames: int = 5 # 默认跳过的帧数，在无法获取视频帧率的情况下使用
+    
     threshold: float = 0.1  # 提高阈值以减少误切
-    min_scene_len: float = 0.5  # 最小场景时间长度(s)
+    min_scene_len: float = 1.0  # 最小场景时间长度(s)
+    
+    screenshot_position: float = 0.95 # 截图在场景中的位置 (默认的时间位置比例)
+    min_screenshot_margin: float = 0.05 # 截图与场景结束的最小间隔 (秒)
+    max_screenshot_margin: float = 0.5 # 截图与场景结束的最大间隔 (秒)
+
     
 def make_pptx(output:str,img_list:list,note_list:list=None):
     prs=Presentation()
@@ -48,32 +58,47 @@ def make_pptx(output:str,img_list:list,note_list:list=None):
 # 1,1,00:00:00.000,0.000,174,00:00:06.960,6.960,174,00:00:06.960,6.960
 # 7,16405,00:10:56.160,656.160,18024,00:12:00.960,720.960,1620,00:01:04.800,64.800
 def time_info(scene_info_csv:str):
+    scene_infos=load_scene_information(scene_info_csv)
+    time_notes=[ f"Time Info: scene {scene_details[0]}, {scene_details[2]}-{scene_details[5]} ({scene_details[9]}s)"
+                for scene_details in scene_infos]
+    return time_notes
+
+def load_scene_information(scene_info_csv:str):
     with open(scene_info_csv) as f:
         f.readline()
         f.readline()
         lines=f.readlines()
     scene_infos=[l.strip().split(",") for l in lines]
-    time_notes=[ f"Time Info: scene {scene_details[0]}, {scene_details[2]}-{scene_details[5]} ({scene_details[9]}s)"
-                for scene_details in scene_infos]
-    return time_notes
+    return scene_infos
  
 def seconds_to_timecode(seconds):
     return f"{int(seconds//3600):02d}:{int(seconds%3600//60):02d}:{seconds%60:06.3f}"
 
 def srt_info(scene_info_csv:str,video_path:str):
-    print("[*] Converting video to text...")
+    
+    class _CustomProgressBar(tqdm.tqdm):
+        def __init__(self, *args, **kwargs):
+            kwargs["desc"]="Transcribing"
+            super().__init__(*args, **kwargs)
+            self._current = self.n  # Set the initial value
+            
+    # Inject into tqdm.tqdm of Whisper, so we can see progress
+    import whisper.transcribe 
+    transcribe_module = sys.modules['whisper.transcribe'].tqdm.tqdm = _CustomProgressBar
 
     import whisper
+
+    print("[*] Converting video to text...")
+
+    # 加载模型
     model = whisper.load_model("turbo")
-    result = model.transcribe(video_path,verbose=True)
+    # 转录音轨
+    result = model.transcribe(video_path,fp16=False,verbose=False)
+    
     segments=result["segments"]
     print("[*] Convert video to srt Done.")
         
-    with open(scene_info_csv) as f:
-        f.readline()
-        f.readline()
-        lines=f.readlines()
-    scene_infos=[l.strip().split(",") for l in lines]
+    scene_infos=load_scene_information(scene_info_csv)
     time_notes=[ f"Time Info: scene {scene_details[0]}, {scene_details[2]}-{scene_details[5]} ({scene_details[9]}s)"
                 for scene_details in scene_infos]
     
@@ -110,63 +135,134 @@ def srt_info(scene_info_csv:str,video_path:str):
         srt_notes[i]+="\n\n" + time_notes[i]
     return srt_notes
 
+def screenshots(scene_info_csv:str,video_path:str,tmp_dir:str):
+    """screenshots from video, per scene per screenshot"""
+    scene_infos=load_scene_information(scene_info_csv)
+    imgs_list=[]
+    img_basename_template="scene{scene_number:0"+str(len(str(len(scene_infos))))+"d}.jpg"
+    for scene in tqdm.tqdm(scene_infos,desc="screenshotting"):
+        scene_number=int(scene[0])
+        start_seconds=float(scene[3])
+        end_seconds=float(scene[6])
+        
+        default_screenshot_seconds=start_seconds+Config.screenshot_position*(end_seconds-start_seconds)
+        screenshot_margin=max(Config.min_screenshot_margin,min(Config.max_screenshot_margin,end_seconds-default_screenshot_seconds))
+        screenshot_seconds=end_seconds-screenshot_margin
+        screenshot_seconds=round(screenshot_seconds,3)
+        
+        dst_path=os.path.join(tmp_dir,img_basename_template.format(scene_number=scene_number))
+        screenshot_cmd=f"ffmpeg -ss {screenshot_seconds} -i '{video_path}' -frames:v 1 -q:v 2 -y '{dst_path}' "
+        if not DEBUG: screenshot_cmd+=" > /dev/null 2>&1"
+        if DEBUG: print(screenshot_cmd)
+        os.system(screenshot_cmd)
+        imgs_list.append(dst_path)
+    return imgs_list
+
+def get_video_fps(video_path:str):
+    # 使用 ffprobe 获取视频信息
+
+    cmd = f"ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=noprint_wrappers=1:nokey=1 {video_path}"
+    result = os.popen(cmd).read().strip()
+    
+    # 解析帧率信息
+    match = re.match(r'(\d+)/(\d+)', result)
+    if match:
+        num, denom = map(int, match.groups())
+        fps = num / denom
+        return fps
+    else:
+        raise ValueError("无法解析视频的帧率信息")
+
+def get_skip_frames(video_path:str):
+    try:
+        # 获取视频的帧率
+        fps = get_video_fps(video_path)-1
+        fps = max(0, fps)
+        
+        # 计算跳过的帧数
+        skip_frames = int(fps * Config.skip_interval)
+        if DEBUG: print(f"[*] 视频帧率: {fps}，跳过帧数: {skip_frames}")
+        return skip_frames
+    except Exception as e:
+        print(f"[!] 获取视频帧率失败，返回默认跳过帧数 skip_frame={Config.skip_frames}: {e}")
+        return Config.skip_frames
+
+def detect_scenes(video_path:str,tmp_dir:str):
+    Config.skip_frames=get_skip_frames(video_path)
+    """detect scenes from video"""
+    scenedetect_cmd = (
+        f" scenedetect -i '{video_path}' -o '{tmp_dir}' "
+        f" -fs {Config.skip_frames} "
+        f" detect-hash --size 16  --threshold {Config.threshold}  --lowpass 2 "
+        f" --min-scene-len {Config.min_scene_len} "
+        f" list-scenes --filename scenes_info.csv "
+    )
+
+    if not DEBUG: scenedetect_cmd+=" 2>&1"
+    if DEBUG: print("[*]"+scenedetect_cmd)
+    os.system(scenedetect_cmd)
+    return os.path.join(tmp_dir,"scenes_info.csv")
+
+def register_signal_handlers(local_vars):
+    
+    def cleanup_temp_dir():
+        tmp_dir=local_vars.get("tmp_dir")
+        if DEBUG:
+            print(f"Not clean tmp_dir for debug:  {tmp_dir}")
+        else:
+            if tmp_dir and os.path.exists(tmp_dir):
+                print(f"[*] Cleaning up temporary directory: {tmp_dir}")
+                shutil.rmtree(tmp_dir)
+    
+    def signal_handler(sig, frame):
+        print(f"[*] Signal {signum} received, cleaning up...")
+        cleanup_temp_dir()
+        raise SystemExit(1)
+        
+    atexit.register(cleanup_temp_dir)
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
 app = typer.Typer()
 
 @app.command()
 def main(
-    input: Annotated[str, typer.Argument(..., help="输入视频文件路径")],
+    video: Annotated[str, typer.Argument(..., help="输入视频文件路径")],
     output: Annotated[Optional[str], typer.Argument(...,help="输出pptx文件路径")] =None,
     srt: bool = typer.Option(False, help="是否生成字幕"),
     
     # srt: Annotated[Optional[bool], typer.Option(False, help="是否生成字幕")]=False
     ):
+    """Convert video to pptx slides"""
+    
+    # 捕获 Ctrl+C 信号
+    
     timestamp=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    fname=os.path.basename(video)
     if output is None:
-        output = os.path.splitext(input)[0]+".pptx"
+        output = os.path.splitext(fname)[0]+".pptx"
         if os.path.exists(output):
-            output = os.path.splitext(input)[0]+f"-{timestamp}.pptx"
-    fname=os.path.basename(input)
-    tmp_output=f"./tmp-v2s-{timestamp}-{fname}"
-    
-    # DEBUG，输出统计信息 
-    # f" -s stat.csv "
+            output = os.path.splitext(fname)[0]+f"-{timestamp}.pptx"
+    tmp_dir=f"./tmp-v2s-{timestamp}-{fname}"
+    os.makedirs(tmp_dir,exist_ok=True)
 
-    # 设置 scenedetect 命令
-    scenedetect_cmd = (
-        f" scenedetect -i '{input}' -o '{tmp_output}' "
-        f" -fs {Config.skip_frames} "
-        f" detect-hash --size 16  --threshold {Config.threshold}  --lowpass 2 "
-        f" --min-scene-len {Config.min_scene_len} "
-        f" list-scenes --filename scenes_info.csv "
-        f" save-images --num-images {Config.num_images} --filename scene\\$SCENE_NUMBER-img\\$IMAGE_NUMBER "
-        f" export-html "
-    )
-    print(scenedetect_cmd)
-    os.system(scenedetect_cmd)
-
-    # 
-        
-    # get the image list
-    imgs_list=os.listdir(tmp_output)
-    imgs_list=[f for f in imgs_list if re.match(rf"scene\d+-img0{Config.num_images-1}.jpg",f)]
-    imgs_list.sort()
-    imgs_list=[os.path.join(tmp_output,f) for f in imgs_list]
+    register_signal_handlers(locals())
     
+    scene_info_csv=detect_scenes(video,tmp_dir)
+
     # get the note list
     if srt:
-        notes_list=srt_info(os.path.join(tmp_output,"scenes_info.csv"),input)
+        notes_list=srt_info(scene_info_csv,video)
     else:
-        time_notes=time_info(os.path.join(tmp_output,"scenes_info.csv"))
+        time_notes=time_info(scene_info_csv)
         notes_list=time_notes
     
+    # screenshot from scenes
+    imgs_list = screenshots(scene_info_csv,video_path=video,tmp_dir=tmp_dir)
+
     # convert to pptx
     make_pptx(output,imgs_list,notes_list)
     
-    if DEBUG:
-        print(f"未删除临时文件夹： {tmp_output}")
-    else:
-        shutil.rmtree(tmp_output,ignore_errors=True)
-        print(f"删除临时文件夹 {tmp_output}")
     print(f"已生成 {output}")
     
 if __name__ == "__main__":
